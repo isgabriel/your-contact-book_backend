4a91be99d6a2d86f128666a56cb0830b227a7e23
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2023-08-25 17:27:07 -0300

    feat: vercel file

--- src/services/users/updateUser.service.ts
+++ src/services/users/updateUser.service.ts
@@ -1,21 +1,43 @@
 import { Repository } from "typeorm";
 import { AppDataSource } from "../../data-source";
 import { User } from "../../entities/user.entities";
-import { noPasswordNoContactsUserSchema } from "../../schemas/users.schemas";
+import {
+    noPasswordNoContactsUserSchema,
+    userSchemaResponse,
+} from "../../schemas/users.schemas";
+import { TUserPatch, TUserResponse } from "../../interfaces/users.interfaces";
 
-const updateUserService = async (payload: object, oldData: User) => {
+const updateUserService = async (
+    payload: TUserPatch,
+    id: number
+): Promise<TUserResponse> => {
     const userRepository: Repository<User> = AppDataSource.getRepository(User);
 
-    const updatedUser = userRepository.create({
-        ...oldData,
-        ...payload,
+    const oldData: User | null = await userRepository.findOneBy({
+        id: id,
     });
 
-    await userRepository.save(updatedUser);
+    const { ...userDataId } = payload;
 
-    const serializedUser = noPasswordNoContactsUserSchema.parse(updatedUser);
+    const user: User = userRepository.create({
+        ...oldData,
+        ...userDataId,
+    });
+    await userRepository.save(user);
 
-    return serializedUser;
+    const parse: TUserResponse = userSchemaResponse.parse(user);
+    return parse;
+
+    // const updatedUser = userRepository.create({
+    //     ...oldData,
+    //     ...payload,
+    // });
+
+    // await userRepository.save(updatedUser);
+
+    // const serializedUser = noPasswordNoContactsUserSchema.parse(updatedUser);
+
+    // return serializedUser;
 };
 
 export { updateUserService };
